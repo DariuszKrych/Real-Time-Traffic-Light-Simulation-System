d6bbef962c4b27c4164f071dcc022b589de7630a
DariuszKrych <darek.krych@gmail.com>
2026-03-04 14:34:56 +0100

    Camera + Refactoring
    Added camera movement and zooming.
    
    Set the junction roads and accompanying dotted lines to be of a defined length and not just based on screen dimensions.
    
    Refactored the code so that the while loop runs within the main.py for easier access for other future sceipts while the rendering specific code is kept in the render.py as much as possible including the movement/zoom controls for the camera as well.

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from render import *
-
+from render import create_window, create_junction_renderer, update_camera_from_input
+import sdl3
+import ctypes
 
 def main():
     window_dimensions, window, renderer = create_window()
-    draw_junction(window_dimensions, window, renderer)
+    window_width, window_height = window_dimensions
 
+    # Create the drawing function for the junction with all methods and attributes saved for it to access
+    draw_scene = create_junction_renderer(window_dimensions)
+
+    # Camera and zoom details
+    camera_x, camera_y,camera_speed = 0, 0, 5
+    zoom, min_zoom, max_zoom, zoom_speed = 1.0, 0.5, 2.0, 0.01
+
+    event = sdl3.SDL_Event()
+    running = True
+
+    while running:
+        # Process events
+        while sdl3.SDL_PollEvent(ctypes.byref(event)):
+            if event.type == sdl3.SDL_EVENT_QUIT:
+                running = False
+
+        # Get keyboard state and update camera
+        keys = sdl3.SDL_GetKeyboardState(None)
+        camera_x, camera_y, zoom = update_camera_from_input(
+            keys, camera_x, camera_y, zoom,
+            camera_speed, zoom_speed, min_zoom, max_zoom,
+            window_width, window_height
+        )
+
+        # Draw the junction with current camera and zoom
+        draw_scene(renderer, camera_x, camera_y, zoom)
+
+        # Simple delay to maintain ~60 FPS (16ms per frame)
+        sdl3.SDL_Delay(16)
+
+    # Clean up resources
+    sdl3.SDL_DestroyRenderer(renderer)
+    sdl3.SDL_DestroyWindow(window)
+    sdl3.SDL_Quit()
 
 if __name__ == "__main__":
     main()
--- render.py
+++ render.py
@@ -14,26 +14,29 @@ def create_window():
     return window_dimensions, window, renderer
 
 
-def draw_junction(window_dimensions, sdl_window, sdl_renderer):
+def create_junction_renderer(window_dimensions):
     window_width, window_height = window_dimensions
-    window, renderer = sdl_window, sdl_renderer
-
-    def draw_rectangle(x_coord, y_coord, rect_width, rect_height):
-        rect = sdl3.SDL_FRect(x_coord, y_coord, rect_width, rect_height)
-        sdl3.SDL_RenderFillRect(renderer, ctypes.byref(rect))
-
 
     # Common road details
+    road_length = 2000
     road_width = 200
     road_rgba_colour = [166, 156, 144, 255]
 
     # Defining horizontal road details
-    horiz_road_width, horiz_road_height = [window_width, road_width]
-    horiz_road_pos_x, horiz_road_pos_y = (window_width-horiz_road_width)/2, (window_height-horiz_road_height)/2
+    horiz_road_width = road_length
+    horiz_road_height = road_width
+    horiz_road_pos_x = (window_width - horiz_road_width) / 2
+    horiz_road_pos_y = (window_height - horiz_road_height) / 2
 
     # Defining vertical road details
-    vert_road_width, vert_road_height = [road_width, window_height]
-    vert_road_pos_x, vert_road_pos_y = (window_width-vert_road_width)/2, (window_height-vert_road_height)/2
+    vert_road_width = road_width
+    vert_road_height = road_length
+    vert_road_pos_x = (window_width - vert_road_width) / 2
+    vert_road_pos_y = (window_height - vert_road_height) / 2
+
+    # Intersection centre (world coordinates)
+    cx = horiz_road_pos_x + horiz_road_width / 2
+    cy = horiz_road_pos_y + horiz_road_height / 2
 
     # Common lane line details
     lane_line_rgba_colour = [255, 255, 255, 255]
@@ -43,78 +46,79 @@ def draw_junction(window_dimensions, sdl_window, sdl_renderer):
     lane_line_length = 50
     lane_line_gap = 30
     end_dist_before_junction = 20
-    # Drawing dotted lane lines
-    def dotted_lane_line_drawing():
-        # Yes, the interpreter is complaining that the code is repeated, although it is worth it as otherwise it
-        # would be incredibly illegible whereas now it is immediately very clear how all the dotted lane lines are being drawn.
-        # Yes DRY is important although keeping maximum simplicity while creating the same features is also incredibly important.
 
-        current_pos_y = (window_height - lane_line_thickness) / 2
+    # ---- internal drawing helpers ----
+    def draw_rectangle(renderer, x_coord, y_coord, rect_width, rect_height, camera_x, camera_y, zoom):
+        # Translate world coordinates by camera offset and scale by zoom
+        translated_x = x_coord - camera_x
+        translated_y = y_coord - camera_y
+        scaled_x = translated_x * zoom
+        scaled_y = translated_y * zoom
+        scaled_w = rect_width * zoom
+        scaled_h = rect_height * zoom
+        rect = sdl3.SDL_FRect(scaled_x, scaled_y, scaled_w, scaled_h)
+        sdl3.SDL_RenderFillRect(renderer, ctypes.byref(rect))
 
-        # Horizontal left road lane lines. (West)
-        start_pos_x, end_pos_x = 0, ((window_width-road_width)/2) - end_dist_before_junction
-        current_pos_x = start_pos_x + lane_line_gap
-        while (current_pos_x+lane_line_length) < end_pos_x:
-            draw_rectangle(current_pos_x, current_pos_y, lane_line_length, lane_line_thickness)
-            current_pos_x = current_pos_x + lane_line_gap + lane_line_length
+    def draw_dotted_lines(renderer, camera_x, camera_y, zoom):
+        # West approach (left side of horizontal road)
+        start_x = cx - road_length / 2
+        end_x = cx - road_width / 2 - end_dist_before_junction
+        y = cy - lane_line_thickness / 2
+        current_x = start_x + lane_line_gap
+        while (current_x + lane_line_length) <= end_x:
+            draw_rectangle(renderer, current_x, y, lane_line_length, lane_line_thickness, camera_x, camera_y, zoom)
+            current_x = current_x + lane_line_gap + lane_line_length
 
-        # Horizontal right road lane lines. (East)
-        start_pos_x, end_pos_x = window_width, ((window_width+road_width)/2) + end_dist_before_junction
-        current_pos_x = start_pos_x - lane_line_gap - lane_line_length
-        while current_pos_x > end_pos_x:
-            draw_rectangle(current_pos_x, current_pos_y, lane_line_length, lane_line_thickness)
-            current_pos_x = current_pos_x - lane_line_gap - lane_line_length
+        # East approach (right side of horizontal road)
+        start_x = cx + road_length / 2
+        end_x = cx + road_width / 2 + end_dist_before_junction
+        current_x = start_x - lane_line_gap - lane_line_length
+        while current_x >= end_x:
+            draw_rectangle(renderer, current_x, y, lane_line_length, lane_line_thickness, camera_x, camera_y, zoom)
+            current_x = current_x - lane_line_gap - lane_line_length
 
-        current_pos_x = (window_width-lane_line_thickness)/2
+        # North approach (top side of vertical road)
+        start_y = cy - road_length / 2
+        end_y = cy - road_width / 2 - end_dist_before_junction
+        x = cx - lane_line_thickness / 2
+        current_y = start_y + lane_line_gap
+        while (current_y + lane_line_length) <= end_y:
+            draw_rectangle(renderer, x, current_y, lane_line_thickness, lane_line_length, camera_x, camera_y, zoom)
+            current_y = current_y + lane_line_gap + lane_line_length
 
-        # Vertical top road lane lines. (North)
-        start_pos_y, end_pos_y = 0, ((window_height-road_width)/2) - end_dist_before_junction
-        current_pos_y = start_pos_y + lane_line_gap
-        while (current_pos_y + lane_line_length) < end_pos_y:
-            draw_rectangle(current_pos_x, current_pos_y, lane_line_thickness, lane_line_length)
-            current_pos_y = current_pos_y + lane_line_gap + lane_line_length
+        # South approach (bottom side of vertical road)
+        start_y = cy + road_length / 2
+        end_y = cy + road_width / 2 + end_dist_before_junction
+        current_y = start_y - lane_line_gap - lane_line_length
+        while current_y >= end_y:
+            draw_rectangle(renderer, x, current_y, lane_line_thickness, lane_line_length, camera_x, camera_y, zoom)
+            current_y = current_y - lane_line_gap - lane_line_length
 
-        # Vertical bottom road lane lines. (South)
-        start_pos_y, end_pos_y = window_height, ((window_height+road_width)/2) + end_dist_before_junction
-        current_pos_y = start_pos_y - lane_line_gap - lane_line_length
-        while current_pos_y > end_pos_y:
-            draw_rectangle(current_pos_x, current_pos_y, lane_line_thickness, lane_line_length)
-            current_pos_y = current_pos_y - lane_line_gap - lane_line_length
-
-    # Drawing solid stop lines on the left lanes of each approach
-    def stop_lane_line_drawing():
+    def draw_stop_lines(renderer, camera_x, camera_y, zoom):
         line_length = road_width / 2  # half the road width
 
         # North approach (top)
-        north_y = ((window_height - road_width) / 2) - end_dist_before_junction - lane_line_thickness
-        north_x = window_width / 2
-        draw_rectangle(north_x, north_y, line_length, lane_line_thickness)
+        north_y = cy - road_width / 2 - end_dist_before_junction - lane_line_thickness
+        north_x = cx
+        draw_rectangle(renderer, north_x, north_y, line_length, lane_line_thickness, camera_x, camera_y, zoom)
 
         # South approach (bottom)
-        south_y = ((window_height + road_width) / 2) + end_dist_before_junction
-        south_x = (window_width - road_width) / 2
-        draw_rectangle(south_x, south_y, line_length, lane_line_thickness)
+        south_y = cy + road_width / 2 + end_dist_before_junction
+        south_x = cx - road_width / 2
+        draw_rectangle(renderer, south_x, south_y, line_length, lane_line_thickness, camera_x, camera_y, zoom)
 
         # East approach (right)
-        east_x = ((window_width + road_width) / 2) + end_dist_before_junction
-        east_y = window_height / 2
-        draw_rectangle(east_x, east_y, lane_line_thickness, line_length)
+        east_x = cx + road_width / 2 + end_dist_before_junction
+        east_y = cy
+        draw_rectangle(renderer, east_x, east_y, lane_line_thickness, line_length, camera_x, camera_y, zoom)
 
         # West approach (left)
-        west_x = ((window_width - road_width) / 2) - end_dist_before_junction - lane_line_thickness
-        west_y = (window_height - road_width) / 2
-        draw_rectangle(west_x, west_y, lane_line_thickness, line_length)
+        west_x = cx - road_width / 2 - end_dist_before_junction - lane_line_thickness
+        west_y = cy - road_width / 2
+        draw_rectangle(renderer, west_x, west_y, lane_line_thickness, line_length, camera_x, camera_y, zoom)
 
-
-    event = sdl3.SDL_Event()
-    running = True
-
-    while running:
-
-        while sdl3.SDL_PollEvent(ctypes.byref(event)):
-            if event.type == sdl3.SDL_EVENT_QUIT:
-                running = False
-
+    # ---- main drawing function (returned) ----
+    def draw_scene(renderer, camera_x, camera_y, zoom):
         # Drawing background colour
         sdl3.SDL_SetRenderDrawColor(renderer, 172, 205, 111, 255)
         sdl3.SDL_RenderClear(renderer)
@@ -122,23 +126,62 @@ def draw_junction(window_dimensions, sdl_window, sdl_renderer):
         # Setting to road colour
         sdl3.SDL_SetRenderDrawColor(renderer, road_rgba_colour[0], road_rgba_colour[1], road_rgba_colour[2], road_rgba_colour[3])
         # Drawing horizontal and vertical roads
-        draw_rectangle(horiz_road_pos_x, horiz_road_pos_y, horiz_road_width, horiz_road_height)
-        draw_rectangle(vert_road_pos_x, vert_road_pos_y, vert_road_width, vert_road_height)
+        draw_rectangle(renderer, horiz_road_pos_x, horiz_road_pos_y, horiz_road_width, horiz_road_height, camera_x, camera_y, zoom)
+        draw_rectangle(renderer, vert_road_pos_x, vert_road_pos_y, vert_road_width, vert_road_height, camera_x, camera_y, zoom)
 
         # Setting to lane line colour
         sdl3.SDL_SetRenderDrawColor(renderer, lane_line_rgba_colour[0], lane_line_rgba_colour[1], lane_line_rgba_colour[2], lane_line_rgba_colour[3])
         # Drawing lane lines
-        dotted_lane_line_drawing()
+        draw_dotted_lines(renderer, camera_x, camera_y, zoom)
         # Drawing stop lines
-        stop_lane_line_drawing()
+        draw_stop_lines(renderer, camera_x, camera_y, zoom)
 
         # Present the frame to the window
         sdl3.SDL_RenderPresent(renderer)
 
-        # Simple delay to maintain ~60 FPS (16ms per frame)
-        sdl3.SDL_Delay(16)
+    return draw_scene
 
-    # Clean up resources
-    sdl3.SDL_DestroyRenderer(renderer)
-    sdl3.SDL_DestroyWindow(window)
-    sdl3.SDL_Quit()
+
+def update_camera_from_input(keys, camera_x, camera_y, zoom, camera_speed, zoom_speed, min_zoom, max_zoom, window_width, window_height):
+    new_camera_x = camera_x
+    new_camera_y = camera_y
+    new_zoom = zoom
+
+    # WASD camera movement
+    if keys[sdl3.SDL_SCANCODE_W]:
+        new_camera_y -= camera_speed  # move camera up
+    if keys[sdl3.SDL_SCANCODE_S]:
+        new_camera_y += camera_speed  # move camera down
+    if keys[sdl3.SDL_SCANCODE_A]:
+        new_camera_x -= camera_speed  # move camera left
+    if keys[sdl3.SDL_SCANCODE_D]:
+        new_camera_x += camera_speed  # move camera right
+
+    # Q/E Zoom inputs (zoom in/out centered on screen)
+    if keys[sdl3.SDL_SCANCODE_Q]:
+        # Zoom in
+        new_zoom = zoom + zoom_speed
+        if new_zoom > max_zoom:
+            new_zoom = max_zoom
+        if new_zoom != zoom:
+            # Compute world point currently at screen centre before changing zoom
+            world_center_x = camera_x + (window_width / 2) / zoom
+            world_center_y = camera_y + (window_height / 2) / zoom
+            new_zoom = new_zoom
+            # Adjust camera so that same world point stays at screen centre
+            new_camera_x = world_center_x - (window_width / 2) / new_zoom
+            new_camera_y = world_center_y - (window_height / 2) / new_zoom
+
+    if keys[sdl3.SDL_SCANCODE_E]:
+        # Zoom out
+        new_zoom = zoom - zoom_speed
+        if new_zoom < min_zoom:
+            new_zoom = min_zoom
+        if new_zoom != zoom:
+            world_center_x = camera_x + (window_width / 2) / zoom
+            world_center_y = camera_y + (window_height / 2) / zoom
+            new_zoom = new_zoom
+            new_camera_x = world_center_x - (window_width / 2) / new_zoom
+            new_camera_y = world_center_y - (window_height / 2) / new_zoom
+
+    return new_camera_x, new_camera_y, new_zoom
